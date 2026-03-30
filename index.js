996b677b30b5f5a33b96be41d57a4ea71d1c4f41
Andres Espinosa Sanchez <mauricioe.freelancer@gmail.com>
2026-03-30 08:29:32 -0500

    Optimizar uso de memoria en Render

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu'],
   },
 });
 
